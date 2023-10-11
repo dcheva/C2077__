06ff2f8ad6e4336f5b338c703ae89caa7880b5b4
Dmytro Cheva <dmytro.cheva@gmail.com>
2023-10-12 01:25:20 +0300

    Autogit-art 12-Oct-23 at  1:25:20

--- art.bat
+++ art.bat
@@ -1,3 +1,6 @@
 del /F /Q "C:\Documents and Settings\Dmytro\AppData\Local\NVIDIA\DXCache\*.bin" "C:\Documents and Settings\Dmytro\AppData\Local\NVIDIA\DXCache\*.toc"
-start C:\Projects\C2077\autogit-this.bat Autogit-this %date% at %time:~0,2%:%time:~3,2%:%time:~6,2%
+pushd C:\Projects\C2077
+git add -A
+git commit -am "Autogit-art %date% at %time:~0,2%:%time:~3,2%:%time:~6,2%"
+git checkout art
 start Cyberpunk2077.exe
